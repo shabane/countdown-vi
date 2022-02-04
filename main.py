406b2f7fc64df4dbea26c2174a2453001c65abfa
shabane <m.mohamadshabane@gmail.com>
2022-02-04 21:41:45 +0100

    now the user will check if is admin of the channel or not,
    	if so, send the countdown,
    	if not, reply that the uesr is not the admin

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
 
-from ast import arg
 import threading
 import time
+from traceback import print_tb
 from humanfriendly import format_timespan as left
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import telegram
 
 print('starting.')
 
-# just get a token from telegram and put it here
-token = ""
+token = "token removed, got yours"
 
 
 
-# chn = None
+
 toggeled = True
-# interval = 1
+bot = telegram.Bot(token=token)
 
-def timer(second=0, minute=0, hour=0, end_msg:str='', chn=str) -> None:
+def timer(second=0, minute=0, hour=0, end_msg:str='', chn=str):
     
     interval = second + (minute*60) + (hour*3600)  
     
-    # tmp = updater.message.reply_text(f'{left(interval)} {end_msg}', chat_id=chn)
-    bot = telegram.Bot(token=token)
     tmp = bot.send_message(chat_id=chn, text=f'{left(interval)} {end_msg}')
     
     while(interval>0):
@@ -44,17 +40,28 @@ threads = list()
 def sec(update, context):
     global toggeled
     if(toggeled):
-        x = update.message.text
-        h, m, s, msg, chn = x.split(':')
-        # timer(int(s), int(m), int(h), end_msg=msg, updater=update, chn=chn)
+                
+        h, m, s, msg, chn = update.message.text.split(':')
         
         h = int(h)
         m = int(m)
         s = int(s)
+        msg = msg.strip()
+        chn = chn.strip()
+        user = update.message.chat.username
+        adminds = []
         
-        t = threading.Thread(target=timer, args=(s, m, h, msg, chn,))
-        threads.append(t)
-        t.start()
+        
+        ## getting admins username
+        for i in bot.get_chat_administrators(chat_id=chn):
+            adminds.append(i.user.username)
+        
+        if(user in adminds):
+            t = threading.Thread(target=timer, args=(s, m, h, msg, chn,))
+            threads.append(t)
+            t.start()
+        else:
+            update.message.reply_text('you are not the admin of this chat')
 
 
 def start(update, context):
@@ -62,17 +69,14 @@ def start(update, context):
     toggeled = True
     update.message.reply_text(
 """
-well, this bot will countdown to a specified time for you. just send the time.
-
-at first you should register your channel, then add this bot to your channel.
+well, this bot will countdown until a specified interval for a channel or group.
 
 specify a exact interval to countdown.
-1st column is hour, 2st is minute, 3th column is second, 4th column is for message and the last column is for the channel you want to send the countdown to.;
 
-# hour:minute:second:your message
+# hour:minute:second:your message:@ChannelID
 for example for one hour and 4 minute and 30 second:
 
-1:0:30:to start meeting:@ChannelName
+1:0:30:to start meeting:@ChannelID
 """)
     print(update.message.text)
 
